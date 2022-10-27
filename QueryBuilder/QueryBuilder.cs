7f25435ff4d16f7bfbb962d7c5b9e95683c3a47f
Dylan Pogue <POGUEDJ@ETSU.EDU>
2022-10-27 17:47:03 -0400

    2ndupdate1

--- QueryBuilder/QueryBuilder.cs
+++ QueryBuilder/QueryBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
@@ -29,10 +30,6 @@ namespace QueryBuilder.Models
         /// driver; when that using statement is complete, our Sqlite
         /// connection will be closed automatically
         /// </summary>
-        public void Dispose()
-        {
-            connection.Dispose();
-        }
 
         public List<T> ReadAll<T>() where T : IClassModel, new()
         {
@@ -54,6 +51,110 @@ namespace QueryBuilder.Models
                 datas.Add(data);
             }
             return datas;
+        }
+
+        //Create
+        public void Create<T>(T obj)
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            List<string> values = new List<string>();
+            List<string> names = new List<string>();
+
+            foreach(PropertyInfo property in properties)
+            {
+                if (property.PropertyType == typeof(string))
+                {
+                    values.Add("\"" + property.GetValue(obj) + "\"");
+                }
+                else
+                    values.Add(property.GetValue(obj).ToString());
+
+                names.Add(property.Name);
+            }
+
+            StringBuilder sbValues = new StringBuilder();
+            StringBuilder sbNames = new StringBuilder();
+
+            for(int i = 0; i < values.Count; i++)
+            {
+                if(i == values.Count - 1)
+                {
+                    sbValues.Append($"{values[i]}");
+                    sbNames.Append($"{names[i]}");
+                }
+                else
+                {
+                    sbValues.Append($"{values[i]}, ");
+                    sbNames.Append($"{names[i]}, ");
+                }
+            }
+
+            var command = connection.CreateCommand();
+            command.CommandText = $"INSERT INTO {typeof(T).Name} ({sbNames}) Values ({sbValues})";
+
+            var insert = command.ExecuteNonQuery();
+
+
+        }
+
+        //Update
+        public void Update<T>(T obj)
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            List<string> values = new List<string>();
+            List<string> names = new List<string>();
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.PropertyType == typeof(string))
+                {
+                    values.Add("\"" + property.GetValue(obj) + "\"");
+                }
+                else
+                    values.Add(property.GetValue(obj).ToString());
+
+                names.Add(property.Name);
+            }
+
+            StringBuilder sbValues = new StringBuilder();
+            StringBuilder sbNames = new StringBuilder();
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (i == values.Count - 1)
+                {
+                    sbValues.Append($"{values[i]}");
+                    sbNames.Append($"{names[i]}");
+                }
+                else
+                {
+                    sbValues.Append($"{values[i]}, ");
+                    sbNames.Append($"{names[i]}, ");
+                }
+            }
+
+            var command = connection.CreateCommand();
+            command.CommandText = $"UPDATE {typeof(T).Name} ({sbNames}) Values ({sbValues})";
+
+            var update = command.ExecuteNonQuery();
+
+
+        }
+
+        //Delete
+        public void Delete<T>(T obj) where T : IClassModel
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = $"DELETE FROM {typeof(T).Name} WHERE Id = {obj.Id}";
+
+            var delete = command.ExecuteNonQuery();
+        }
+
+        public void Dispose()
+        {
+            connection.Dispose();
         }
     }
 }
